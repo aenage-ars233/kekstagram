a515f6802996c7be117dcd433a03596135f62fb1
Arseniy <aenadegnow@gmail.com>
2025-07-09 12:57:50 +0500

    Разбил код на модули

---
+++ js/modules/util.js
@@ -0,0 +1,17 @@
+function getRandomPositiveInteger (a, b) {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+
+  return Math.floor(result);
+}
+
+function getRandomArrayElement (elements) {
+  return elements[getRandomPositiveInteger(0, elements.length - 1)];
+}
+
+function checkStringLength (string, length) {
+  return string.length <= length;
+}
+
+export {getRandomArrayElement, getRandomPositiveInteger, checkStringLength};
